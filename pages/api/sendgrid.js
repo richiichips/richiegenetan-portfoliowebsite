ab82d1af6c8e709deea71e85a40a42a2cc2c2cfd
Richie Tan <richiegenetan14@gmail.com>
2021-11-06 21:37:01 +0800

    change "email" to recipient

--- pages/api/sendgrid.js
+++ pages/api/sendgrid.js
@@ -5,7 +5,7 @@ sendgrid.setApiKey(process.env.SENDGRID_API_KEY);
 async function sendEmail(req, res) {
   try {
     await sendgrid.send({
-      to: "richiegenetan14@gmail.com", // Your email where you'll receive emails
+      to: "richietan.taktyl@gmail.com", // Your email where you'll receive emails
       from: "hello@richiegenetan.com", // your website email address here
       subject: `[Lead from Richie Gene Tan's Portfolio Website] : ${req.body.subject}`,
       html: `<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
